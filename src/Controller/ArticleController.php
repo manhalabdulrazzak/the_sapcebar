3ee3116d0baf27e97d293ce116ef29e02cd55089
manhal abdulraazak <cda19-ama@ccicampus.fr>
2020-06-10 12:29:28 +0200

    add new

--- src/Controller/ArticleController.php
+++ src/Controller/ArticleController.php
@@ -10,10 +10,10 @@ class ArticleController extends AbstractController
     /**
      * @Route("/", name="article")
      */
-    public function index()
+    public function show ($slug)
     {
-        return $this->render('article/index.html.twig', [
-            'controller_name' => 'ArticleController',
+        return $this->render('article/show.html.twig',[
+            'title'=> ucwords(str_replace('-','',$slug))
         ]);
     }
 }
